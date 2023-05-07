2ffb527f8525d88af18e5760f3c006819a7b2ac0
Alexander Mirvis <45396441+LynxGeekNYC@users.noreply.github.com>
2023-05-06 23:02:13 -0400

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,33 @@
+<?php
+// Set the UPX payment amount in micro UPX
+$upx_amount = 1000000;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Validate UPX payment using Upland API
+    $access_token = 'YOUR_ACCESS_TOKEN';
+    $headers = array('Authorization: Bearer ' . $access_token);
+    $url = 'https://api.upland.me/v1/upx/checkbalance';
+    $response = file_get_contents($url, false, stream_context_create(array(
+        'http' => array(
+            'method' => 'GET',
+            'header' => implode("\r\n", $headers)
+        )
+    )));
+    $upx_balance = json_decode($response, true)['balance'];
+    if ($upx_balance < $upx_amount) {
+        echo "Please pay at least " . $upx_amount . " UPX to submit the form.";
+        exit();
+    }
+    // Process the form submission
+    // ...
+    echo "Thank you for submitting the form!";
+    exit();
+}
+?>
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+    Name: <input type="text" name="name"><br>
+    Email: <input type="text" name="email"><br>
+    UPX Payment: <input type="text" name="upx"><br>
+    <input type="submit" value="Submit">
+</form>
